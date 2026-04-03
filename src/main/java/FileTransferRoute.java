ac8414f4913d2adea285695cbc8692758e45e39e
LuisPineda <luis.pineda.revelo@udla.edu.ec>
2026-04-03 15:19:19 -0500

    Primera ruta funcional con Apache Camel

---
+++ src/main/java/FileTransferRoute.java
@@ -0,0 +1,17 @@
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.main.Main;
+
+public class FileTransferRoute extends RouteBuilder {
+    public static void main(String[] args) throws Exception {
+        Main main = new Main();
+        main.configure().addRoutesBuilder(new FileTransferRoute());
+        main.run();
+    }
+
+    @Override
+    public void configure() throws Exception {
+        from("file:/home/luisp/Documents/IntegracionSistemas/camel-lab/input?noop=true")
+                .log("Procesando archivo: ${file:name}")
+                .to("file:/home/luisp/Documents/IntegracionSistemas/camel-lab/output");
+    }
+}
