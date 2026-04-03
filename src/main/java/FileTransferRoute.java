9057a429b2ad5f48eb5329f793230adfacfdbeae
LuisPineda <luis.pineda.revelo@udla.edu.ec>
2026-04-03 16:01:40 -0500

    Ejercicio 4: Se agregan logs y filtros

--- src/main/java/FileTransferRoute.java
+++ src/main/java/FileTransferRoute.java
@@ -11,13 +11,16 @@ public class FileTransferRoute extends RouteBuilder {
     @Override
     public void configure() throws Exception {
         from("file:/home/luisp/Documents/IntegracionSistemas/camel-lab/input?noop=true")
-                .log("Procesando archivo: ${file:name}")
+                .filter(header("CamelFileName").endsWith(".csv"))
+                .filter(header("CamelFileName").contains("ventas"))
+                .filter(header("CamelFileLastModified").isGreaterThanOrEqualTo(simple("${date:now:yyyyMMdd000000000}")))
+                .log("Procesando archivo: ${file:name} | Fecha y hora: ${date:now:dd-MM-yyyy HH:mm:ss} ")
                 .convertBodyTo(String.class)
                 .transform().simple("${body.toUpperCase()}")
                 .to("file:/home/luisp/Documents/IntegracionSistemas/camel-lab/output");
 
         from("file:/home/luisp/Documents/IntegracionSistemas/camel-lab/output")
-            .log("Archivando archivo: ${file:name}")
-            .to("file:/home/luisp/Documents/IntegracionSistemas/camel-lab/archived");
+                .log("Archivando archivo: ${file:name}")
+                .to("file:/home/luisp/Documents/IntegracionSistemas/camel-lab/archived");
     }
 }
